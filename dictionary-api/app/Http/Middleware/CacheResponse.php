31bb22f6e2738820177dcbc26eef9bb5176d247e
Jardelsr <sribeirodel@gmail.com>
2024-12-23 12:02:18 -0300

    configurando docker

--- dictionary-api/app/Http/Middleware/CacheResponse.php
+++ dictionary-api/app/Http/Middleware/CacheResponse.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
 
 class CacheResponse
 {
@@ -12,16 +13,19 @@ class CacheResponse
         $key = md5($request->fullUrl());
 
         if (Cache::has($key)) {
-            $response = Cache::get($key);
+            $cachedContent = Cache::get($key);
+
+            $response = new Response($cachedContent);
             $response->headers->set('x-cache', 'HIT');
             return $response;
         }
 
         $response = $next($request);
+
+        Cache::put($key, $response->getContent(), 3600);
+
         $response->headers->set('x-cache', 'MISS');
         $response->headers->set('x-response-time', microtime(true) - LARAVEL_START);
-        
-        Cache::put($key, $response, 3600); 
 
         return $response;
     }
